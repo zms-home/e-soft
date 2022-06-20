ef66420e05c50d3a53d2cbba65dc5237eb34f6cf
Maxim <56305518+zms-home@users.noreply.github.com>
2022-06-20 08:27:47 +0500

    Update check_response.py

--- 2-check response/check_response.py
+++ 2-check response/check_response.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+#импортируем модули
 from asyncore import write
 from datetime import date, datetime
 from time import time
@@ -6,18 +8,20 @@ import requests
 import datetime
 import json
 
-
+#Записываем в переменную текущую дату
 current_date = str(datetime.datetime.now())
 
 url = 'https://t72.ru'
 
+#Получаем код ответа и записываем его в пременную
 response = requests.get(url, timeout=5).status_code
 
+#Создаем словарь и записываем в него дату и код ответа
 dictionary = {
     "checkTime": current_date,
     "responseCode": response
 }
-
+#В зависимости от кода ответа создаем файл
 if response == 200:
 
     with open("good.json", "a") as f:
